add675240f5546b101239ffe39f5f80d5d94f40e
Jon <100859974+tursunovn@users.noreply.github.com>
2022-03-26 23:28:26 +0500

    Add files via upload

---
+++ models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+
+    def get_absolute_url(self):
+        return reverse('category_list', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return f'Category (pk={self.pk}, title{self.title})'
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = 'Категории'
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название ')
+    content = models.TextField(blank=True, verbose_name='Описание')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='Дата изменения')
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True, verbose_name='Изображение')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+
+    def get_absolute_url(self):
+        return reverse('article_detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+    def __repr__(self):
+        return f'Article (pk={self.pk}, title{self.title})'
+
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = 'Статьи'
+
+from django.contrib.auth.models import User
+
+class Comment(models.Model):
+    text = models.TextField(verbose_name='Комментарии')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name='Статья')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return f'{self.pk} - {self.created_at} - {self.author}'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+
+        ordering = ['created_at']
